b5bffcd6f743118d1b3443e525dc88fad7cd40a2
Connor White <connorwhitepdx@gmail.com>
2023-08-31 16:04:46 -0700

    further progress

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,26 +1,36 @@
 import React, {useState, useRef} from 'react';
 import { NavLink } from 'react-router-dom';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
 import gsap from 'gsap';
 import './index.css';
 
 export default function Menu() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <div className="menu">
             <div className='logo'>
-                <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt="logo" />
+                <img src="./images/logo.png" alt="logo" />
+            </div>
+            <div className='menu-btn'>   
+                <MenuRoundedIcon onClick={() => setMenuOpen(!menuOpen)} />
             </div>
             <div className='links'>
                 <div className='nav'>
-                    <NavLink to='/'>About</NavLink>
-                    <NavLink to='/about'>Offerings</NavLink>
-                    <NavLink to='/contact'>Testimonials</NavLink>
-                    <NavLink to='/projects'>Connect</NavLink>
+                    <NavLink className='navlink' to='/'>About</NavLink>
+                    <NavLink className='navlink' to='/about'>Offerings</NavLink>
+                    <NavLink className='navlink' to='/contact'>Testimonials</NavLink>
+                    <NavLink className='navlink' to='/projects'>Connect</NavLink>
                 </div>
                 <div className='social'>
-                    {/* <a href="mailto:connorwhitepdx@gmail.com" className='link'><EmailOutlinedIcon /></a>
-                    <a href="https://www.instagram.com/connorwhite.online" target="_blank" rel="noopener noreferrer" className='link'><InstagramIcon /></a> */}
+                    <a href="mailto:connorwhitepdx@gmail.com" className='link'><EmailOutlinedIcon /></a>
+                    <a href="https://www.instagram.com/connorwhite.online" target="_blank" rel="noopener noreferrer" className='link'><InstagramIcon /></a>
                 </div>
             </div>
+            
         </div>
     )
 }
